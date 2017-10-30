7ff7a5ec669066bd4b35d3ad2260a0b4c50a5b57
Daan Sprenkels <hello@dsprenkels.com>
2017-10-30 14:31:17 +0100

    Add -Wno-sign-compare to build script
    This is done to suppress the comparison-errors that are generated
    because of the concise comparisons in tweetnacl.c.

--- build.rs
+++ build.rs
@@ -4,5 +4,6 @@ fn main() {
     let sources = ["sss/sss.c", "sss/hazmat.c", "sss/randombytes.c", "sss/tweetnacl.c"];
     cc::Build::new()
         .files(sources.iter())
+        .flag("-Wno-sign-compare") // Suppress sign warnings in tweetnacl.c
         .compile("libsss.a");
 }
