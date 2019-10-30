03a8bbbada55c7034ad5c66cf3c054ca8459d75e
Marc Nijdam <marc@helium.com>
2019-10-30 08:21:47 -0700

    Add have_libsodium feature to disable interna randombytes implementation

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 extern crate cc;
 
 fn main() {
+    #[cfg(not(feature="have_libsodium"))]
     let sources = ["sss/sss.c", "sss/hazmat.c", "sss/randombytes.c", "sss/tweetnacl.c"];
+    #[cfg(feature="have_libsodium")]
+    let sources = ["sss/sss.c", "sss/hazmat.c", "sss/tweetnacl.c"];
     cc::Build::new()
         .files(sources.iter())
         .flag("-Wno-sign-compare") // Suppress sign warnings in tweetnacl.c
